23b18ed902b1d12d0677c805171b6344a2101069
Yoann Robinet <yoann@yopbox.com>
2021-09-08 11:29:42 +0200

    correctif spl_autoload

--- controllers/Router.php
+++ controllers/Router.php
@@ -8,7 +8,7 @@ class Router {
         try {
             spl_autoload_register(function($class) {
                 require_once('../models/'.$class.'.php');
-            })
+            });
 
             $url = '';
 
