0aa926318d4644a33306251725de504702d24f6a
Alexis Migewant <alexis.migewant@tpc26.cpe.fr>
2021-09-08 11:23:27 +0200

    Initialisation du Model

---
+++ models/Model.php
@@ -0,0 +1,32 @@
+<?php
+
+abstract class Model {
+    private static $_bdd;
+
+    private static function setBdd() {
+        $host = '';
+        $dbname = '';
+        $user = '';
+        $pass = '';
+        self::$_bdd = new PDO('mysql:host='.$host.';dbname='.$dbname.';charset=utf8', $user, $pass);
+        self::$_bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+    }
+
+    protected function getBdd() {
+        if(self::$_bdd == null)
+            self::setBdd();
+
+        return self::$_bdd
+    }
+
+    protected function getAll($table, $obj) {
+        $var = [];
+        $req = $this->getBdd()->prepare('SELECT * FROM '.$table.' ORDER BY id asc');
+        $req->execute();
+        while($data = $req->fetch(PDO::FETCH_ASSOC)) {
+            $var = new $obj($data);
+        }
+        return $var;
+        $req->closeCursor();
+    }
+}
