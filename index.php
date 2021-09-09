4319b94410c75a62352775b5817d92f7ff6feb85
Alexis MIGEWANT <alexis.migewant@cpe.fr>
2021-09-09 15:24:24 +0200

    Remise au propre

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
 <?php
-
 require_once('controllers/Router.php');
-
-
-require_once('models/Model.php');
-
 $router = new Router();
 $router->routeReq();
-
-$models = new Model();
-$models->getAll('symptome', '');
-
