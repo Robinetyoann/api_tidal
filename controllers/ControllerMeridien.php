dfa8ffce763ff4409eb81c3ac01749ea7239ce12
Alexis MIGEWANT <alexis.migewant@cpe.fr>
2021-09-15 08:38:09 +0200

    feat: create fecth for meridiens

---
+++ controllers/ControllerMeridien.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once('./models/MeridienManager.php');
+
+class ControllerMeridien {
+    private $_meridienManager;
+    private $_meridiens;
+
+    public function __construct($url) {
+        $this->meridien();
+    }
+
+    private function meridien() {
+        $this->_meridienManager = new MeridienManager;
+        $array = [
+            'success' => true,
+            'message' => 200,
+            'data' => $this->_meridienManager->getMeridiens()
+        ];
+        echo json_encode($array);
+    }
+}
