de85d59a0d92c01e3b1e43fa1aedbd14499d8708
Alexis MIGEWANT <alexis.migewant@cpe.fr>
2021-09-15 09:49:19 +0200

    feat: test populate

--- models/SymptomeManager.php
+++ models/SymptomeManager.php
@@ -7,4 +7,8 @@ class SymptomeManager extends Model  {
     public function getSymptomes() {
         return $this->getAll('symptome');
     }
+
+    public function populateSymptomes() {
+        return $this->populate(['test', 'test']);
+    }
 }
