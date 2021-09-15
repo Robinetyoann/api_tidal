e8ba4290f14bcd32212223988d7908121952afe8
Alexis MIGEWANT <alexis.migewant@cpe.fr>
2021-09-15 10:30:04 +0200

    fix: remove plural

--- controllers/Router.php
+++ controllers/Router.php
@@ -4,11 +4,6 @@ class Router {
     private $_ctrl;
     private $_view;
 
-    protected function is_plural($string) {
-        $lastLetter = strlen($string) - 1;
-        return $string[$lastLetter] === 's' ? true : false;
-    }
-
     public function routeReq() {
         try {
             spl_autoload_register(function($class) {
@@ -20,8 +15,7 @@ class Router {
             if(isset($_GET['url'])) {
                 $url = explode('/', filter_var($_GET['url']), FILTER_SANITIZE_URL);
 
-                $this->is_plural(strtolower($url[0])) ? $controller = ucfirst(strtolower(substr($url[0], 0, -1))) : $controller = ucfirst(strtolower($url[0]));
-                
+                $controller = ucfirst(strtolower($url[0]));
                 $controllerClass = 'Controller'.$controller;
                 $controllerFile = './controllers/'.$controllerClass.'.php';
 
