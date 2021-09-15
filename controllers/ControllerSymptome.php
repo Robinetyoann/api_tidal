ed8e00c1856a51757846dc5a85e8ecf0df7caaa3
Alexis MIGEWANT <alexis.migewant@cpe.fr>
2021-09-15 09:48:19 +0200

    fix: Optimized code

--- controllers/ControllerSymptome.php
+++ controllers/ControllerSymptome.php
@@ -7,16 +7,30 @@ class ControllerSymptome {
     private $_symptomes;
 
     public function __construct($url) {
-        $this->symptomes();
+        $this->_symptomeManager = new SymptomeManager;
+        if(count($url) === 1 && gettype($url[0]) === 'string') {
+            $this->symptomes();
+        } elseif (count($url) > 1 && gettype($url[0]) === 'string') {
+            $this->populate();
+        }
+        
     }
 
     private function symptomes() {
-        $this->_symptomeManager = new SymptomeManager;
         $array = [
             'success' => true,
             'message' => 200,
             'data' => $this->_symptomeManager->getSymptomes()
         ];
         echo json_encode($array);
+    }
+
+    private function populate() {
+        /*$array = [
+            'success' => true,
+            'message' => 200,
+            'data' => $this->_symptomeManager->populateSymptomes()
+        ];
+        echo json_encode($array);*/
     }
 }
