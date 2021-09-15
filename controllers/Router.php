e575832edaadd715786e4d1337385163e53cc1cb
Alexis MIGEWANT <alexis.migewant@cpe.fr>
2021-09-15 08:11:24 +0200

    fix: can have plural or not url

--- controllers/Router.php
+++ controllers/Router.php
@@ -4,10 +4,10 @@ class Router {
     private $_ctrl;
     private $_view;
 
-    /*protected function is_plural($string) {
+    protected function is_plural($string) {
         $lastLetter = strlen($string) - 1;
         return $string[$lastLetter] === 's' ? true : false;
-    }*/
+    }
 
     public function routeReq() {
         try {
@@ -20,11 +20,11 @@ class Router {
             if(isset($_GET['url'])) {
                 $url = explode('/', filter_var($_GET['url']), FILTER_SANITIZE_URL);
 
-                //$this->is_plural(strtolower($url[0])) ? $controller = ucfirst(strtolower(substr($url[0], 0, -1))) : $controller = ucfirst(strtolower($url[0]));
+                $this->is_plural(strtolower($url[0])) ? $controller = ucfirst(strtolower(substr($url[0], 0, -1))) : $controller = ucfirst(strtolower($url[0]));
                 
                 $controllerClass = 'Controller'.$controller;
                 $controllerFile = './controllers/'.$controllerClass.'.php';
-              
+
                 if(file_exists($controllerFile)) {
                     require_once($controllerFile);
                     $this->_ctrl = new $controllerClass($url);
@@ -42,6 +42,7 @@ class Router {
             $array = [
                 'success' => false,
                 'message' => 400,
+                'data' => $e
             ];
             echo json_encode($array);
         }
