c4dcadd6362f2dffb331538d4e83e20aef051ec1
Alexis MIGEWANT <alexis.migewant@cpe.fr>
2021-09-29 08:15:35 +0200

    fix: refactor

--- models/User.php
+++ models/User.php
@@ -1,20 +1,16 @@
 <?php
-
 const CRYPTED_PWD = '$2y$10$n1hpKeYzE5U5SNjext7rO.BsYkkEajGNZCE53G23uh1thZ4mAMG4S';
-class User extends Model
-{
+
+class User extends Model {
     public $_email;
     public $_password;
 
-
-    public function __construct(string $email, string $password)
-    {
+    public function __construct(string $email, string $password) {
         $this->_email = $email;
-        $this->_password = $password; //;;
+        $this->_password = $password;
     }
 
-    public function register()
-    {
+    public function register() {
         try {
             $this->_password = password_hash($this->_password, PASSWORD_BCRYPT);
             $req = $this->getBdd()->prepare('INSERT INTO user (email, password) VALUES ("' . $this->_email . '","' . $this->_password . '")');
@@ -23,23 +19,17 @@ class User extends Model
             return false;
         }
 
-
-
         return true;
     }
-    public function login()
-    {
+
+    public function login() {
         try {
             $req = $this->getBdd()->prepare('SELECT * from user where email="' . $this->_email . '"');
             $req->execute();
             $user = $req->fetch(PDO::FETCH_OBJ);
 
+            return (password_verify($this->_password, $user->password) ? true : false); 
 
-            if (password_verify($this->_password, $user->password)) {
-                return true;
-            } else {
-                return false;
-            }
         } catch (Exception $e) {
             return false;
         }
