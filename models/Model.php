6fda7f56336040fb0f69b07aff663a695f04fcd5
Alexis MIGEWANT <alexis.migewant@cpe.fr>
2021-10-01 12:08:05 +0200

    feat: add join fetch

--- models/Model.php
+++ models/Model.php
@@ -21,7 +21,7 @@ abstract class Model {
 
     protected function getAll($table) {
         $var = [];
-        $req = $this->getBdd()->prepare('SELECT * FROM '.$table);
+        $req = $this->getBdd()->prepare('SELECT * FROM ' . $table);
         $req->execute();
 
         while ($data = $req->fetch(PDO::FETCH_ASSOC)) {
@@ -31,9 +31,21 @@ abstract class Model {
         $req->closeCursor();
     }
 
-    protected function populate($array) {
-        foreach ($array as $value) {
-            echo $value;
+    protected function populate($arrayDest, $arraySource) {
+        $var = [];
+        $query = 'SELECT * FROM ' . $arraySource['table'] . ' ';
+        foreach ($arrayDest as $key => $value) {
+            $joinQuery = 'JOIN ' .$value->table. ' ON ' .$value->table. '.' .$value->id. '=' .$arraySource['table']. '.' .$arraySource['id'] . ' ';
+            $query .= $joinQuery;
         }
+        
+        $req = $this->getBdd()->prepare('SELECT * FROM symptome JOIN symptPatho ON symptPatho.idS=symptome.idS');
+        $req->execute();
+
+        while ($data = $req->fetch(PDO::FETCH_ASSOC)) {
+            array_push($var, $data);
+        }
+        return $var;
+        $req->closeCursor();
     }
 }
