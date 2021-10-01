dc3ec46574c262402f5c68e4d48c9e3a6ce7afa2
Alexis MIGEWANT <alexis.migewant@cpe.fr>
2021-10-01 11:00:07 +0200

    fix: simplify and take JSON

--- lib/body_request.php
+++ lib/body_request.php
@@ -3,11 +3,12 @@
 function body_request() {
     $string_body = file_get_contents('php://input');
     $string_body =  explode('&', $string_body);
-    $body = [];
+    /*$body = [];
 
     foreach ($string_body  as $elem) {
         $array = explode('=', $elem);
         array_push($body, [$array[0] => $array[1]]);
     }
-    return $body;
+    return $body;*/
+    return json_decode($string_body[0]);
 }
