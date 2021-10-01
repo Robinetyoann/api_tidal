6981130a8fe8b5e0354e81927281dc4422d52b97
Alexis MIGEWANT <alexis.migewant@cpe.fr>
2021-10-01 12:08:17 +0200

    feat: add join fetch

--- models/SymptomeManager.php
+++ models/SymptomeManager.php
@@ -4,11 +4,14 @@ require_once('./models/Model.php');
 
 class SymptomeManager extends Model  {
 
+    private static $_table = 'symptome';
+    private static $_id = 'idS';
+
     public function getSymptomes() {
-        return $this->getAll('symptome');
+        return $this->getAll(self::$_table);
     }
 
     public function populateSymptomes($array) {
-        return $this->populate($array);
+        return $this->populate($array, ['table' => self::$_table, 'id' => self::$_id]);
     }
 }
