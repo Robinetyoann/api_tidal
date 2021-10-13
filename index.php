070ec4cbb6fcccf7c8e4cc560ce7375533e27613
Alexis MIGEWANT <alexis.migewant@cpe.fr>
2021-10-13 09:11:23 +0200

    fix: allow cors

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header("Access-Control-Allow-Headers: X-Requested-With");
 require_once('controllers/Router.php');
 require_once('models/Model.php');
 
