59be773519d3a66d72f0126c2fd39d67f9c5bb91
Alexis MIGEWANT <alexis.migewant@cpe.fr>
2021-10-19 19:59:17 +0200

    feat: add implementation for joining tabls

--- models/PathologieManager.php
+++ models/PathologieManager.php
@@ -4,7 +4,17 @@ require_once('./models/Model.php');
 
 class PathologieManager extends Model  {
 
+    private static $_table = 'patho';
+
     public function getPathologies() {
-        return $this->getAll('patho');
+        return $this->getAll(self::$_table);
+    }
+
+    public function getLinkSymptomes() {
+        return $this->getAll('symptPatho');
+    }
+
+    public function getSymptomes() {
+        return $this->getAll('symptome');
     }
 }
