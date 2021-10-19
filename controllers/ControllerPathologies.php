b074720af23f1c589290449be0c23c57c91816bd
Alexis MIGEWANT <alexis.migewant@cpe.fr>
2021-10-19 19:58:16 +0200

    feat: join tables

--- controllers/ControllerPathologies.php
+++ controllers/ControllerPathologies.php
@@ -2,6 +2,7 @@
 
 require_once('./models/PathologieManager.php');
 require_once('./lib/response_json.php');
+require_once('./lib/join_request.php');
 
 class ControllerPathologies {
     private $_pathologieManager;
@@ -9,13 +10,38 @@ class ControllerPathologies {
 
     public function __construct($url) {
         $this->_pathologieManager = new PathologieManager;
-        $this->pathologie();
+
+        if(count($url) !== 1 || gettype($url[0]) !== 'string') {json(500, 'No valid params');}
+
+        $tablesJoin = $_GET['includes'];
+        if(isset($tablesJoin)) {
+            switch ($tablesJoin) {
+                case 'symptomes':
+                    $this->symptomes();
+                    break;
+            };
+        } else {
+            $this->pathologie();
+        }
     }
 
     private function pathologie() {
         try {
             $data = $this->_pathologieManager->getPathologies();
             json(200, $data);
+        } catch (Exception $e) {
+            json(500, 'No data');
+        }
+    }
+
+    private function symptomes() {
+        try {
+            $symptomesLinks = $this->_pathologieManager->getLinkSymptomes();
+            $pathologies = $this->_pathologieManager->getPathologies();
+            $symptomes = $this->_pathologieManager->getSymptomes();
+
+            json(200, linkTables($symptomesLinks, $pathologies, $symptomes, 'idP', 'idS', 'symptomes'));
+            
         } catch (Exception $e) {
             json(500, 'No data');
         }
